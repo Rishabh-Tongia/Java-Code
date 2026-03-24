1100ad88477d17f6a87bd1f509fd1b9c8e84fe12
Rishabh-Tongia <rishabh10tongia@gmail.com>
2026-03-24 23:06:22 +0530

    Solve Leetcode 242 Anagram using 3 approaches

--- ValidAnagram242.java
+++ ValidAnagram242.java
@@ -31,6 +31,24 @@ public class ValidAnagram242 {
 
     //     return false;
     // }
+
+    // using Array Hashing Approach
+    // public boolean isAnagram(String s, String t) {
+    //     if(s.length() != t.length()){
+    //         return false;
+    //     }
+
+    //     int count[] = new int[26];
+    //     for(int i=0;i<s.length();i++){
+    //         count[s.charAt(i)-'a']++;
+    //         count[t.charAt(i)-'a']--;
+    //     }
+
+    //     for(int val : count){
+    //         if(val != 0) return false;
+    //     }
+    //     return true;
+    // }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String s = sc.next();

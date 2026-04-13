a200989d4725aaff9549acedb990ca6637caf318
Rishabh-Tongia <rishabh10tongia@gmail.com>
2026-04-13 19:57:19 +0530

    solve leetcode 242 anagram  using sorting approach

--- Anagram.java
+++ Anagram.java
@@ -22,6 +22,16 @@ public class Anagram {
         return true;
     }
 
+    // public boolean isAnagram(String s, String t) {
+    //     char[] str1 = s.toCharArray();
+    //     char[] str2 = t.toCharArray();
+
+    //     Arrays.sort(str1);
+    //     Arrays.sort(str2);
+
+    //     return Arrays.equals(str1,str2);
+    // }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
